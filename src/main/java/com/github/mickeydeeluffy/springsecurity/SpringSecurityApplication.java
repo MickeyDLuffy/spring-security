92c2893cbecc3008b43527c4923ff11c43ac0cbb
MickeyDLuffy <thefalconslayer@gmail.com>
2022-11-15 01:31:14 +0000

    Completed ldap authentication:

--- src/main/java/com/github/mickeydeeluffy/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/github/mickeydeeluffy/springsecurity/SpringSecurityApplication.java
@@ -2,7 +2,13 @@ package com.github.mickeydeeluffy.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
@@ -11,4 +17,17 @@ public class SpringSecurityApplication {
         SpringApplication.run(SpringSecurityApplication.class, args);
     }
 
+//    @Bean
+//    public LocalContainerEntityManagerFactoryBean containerEntityManagerFactoryBean() {
+//        LocalContainerEntityManagerFactoryBean mb = new LocalContainerEntityManagerFactoryBean();
+//        mb.setDataSource(dataSource());
+//        mb.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+//        return mb;
+//    }
+//    @Bean
+//    public DataSource dataSource() {
+//        DriverManagerDataSource m = new DriverManagerDataSource();
+//        m.setDriverClassName("");
+//        return m;
+//    }
 }
--- src/main/java/com/github/mickeydeeluffy/springsecurity/controller/LdapResource.java
+++ src/main/java/com/github/mickeydeeluffy/springsecurity/controller/LdapResource.java
@@ -3,11 +3,15 @@ package com.github.mickeydeeluffy.springsecurity.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.Properties;
+
 @RestController
 public class LdapResource {
 
     @GetMapping("/ldap")
     public String index() {
+
         return "index";
     }
 }
